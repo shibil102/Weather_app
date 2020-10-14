69468b7e0d34123fc123bc3429acf47566d5c971
mohamed shibin <67529413+shibil102@users.noreply.github.com>
2020-10-14 22:06:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const SearchBox = document.querySelector('.place')
+const SearchButton = document.querySelector('.sub')
+const city = document.querySelector('.city')
+const desc = document.querySelector('.desc')
+const temp = document.querySelector('.temp')
+const firstHead = document.querySelector('.places')
+const secondHead = document.querySelector('.condition')
+const thirdHead = document.querySelector('.temperature')
+const tables = document.querySelector('table')
+
+SearchButton.addEventListener('click', ()=>{
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+SearchBox.value+'&appid=d8527f4d5989591cb73c6e4b594b0f01')
+    .then(response => response.json())
+    .then(data => {
+let nameOfCity = data['name']
+let TempDetails = data['main']['temp']
+let DescriptionValue = data['weather'][0]['description']
+
+let tempincelsius = (TempDetails - 273.15)
+currentTemp = (Math.floor(tempincelsius))
+firstHead.innerHTML = 'CITY'
+secondHead.innerHTML = 'CONDITION'
+thirdHead.innerHTML = 'TEMPERATURE(°C)'
+city.innerHTML = nameOfCity
+desc.innerHTML = DescriptionValue
+temp.innerHTML = currentTemp + '°C'
+tables.style.border = '1px solid #2e2e2e'
+firstHead.style.border = '1px solid #2e2e2e'
+secondHead.style.border = '1px solid #2e2e2e'
+thirdHead.style.border = '1px solid #2e2e2e'
+city.style.border = '1px solid #2e2e2e'
+desc.style.border = '1px solid #2e2e2e'
+temp.style.border = '1px solid #2e2e2e'
+SearchBox.value = ''
+    })
+    .catch(err => alert('city not found'))
+})
